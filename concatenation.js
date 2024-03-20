a3b216e593c99c2481694d569082306321b72b19
Jacob Zuniga <146957812+Jacobz24@users.noreply.github.com>
2024-03-20 13:51:13 +0000

    Concatenation

--- concatenation.js
+++ concatenation.js
@@ -7,13 +7,15 @@ submitButton.addEventListener("click", function() {
     let mood = document.querySelector(".mood").value;
     let food = document.querySelector(".food-name").value;
     let birthYear = document.querySelector(".year").value;
-    let age = 2023 - birthYear; // update to 2024, if necessary
+    let age = 2024 - birthYear; // update to 2024, if necessary
     // 1. Declare a variable to save the value of the city the user is from.
-
+let city = document.querySelector(".city").value;
+  let space = " "
     // 2. Below write your sentences! 
     // - Have them show up in the div with class "results".
     // - Example paragraph: "My name is (myName). I'm feeling (mood) and I absolutely lovveeee eating (food) 😍..."
     // - Try placing your sentences into <p> or <li> tags.
-
+ results.innerHTML = "My name is" + space + myName + "," + space  + "I was born in" + space + birthYear + "," + space + space + "In" + space + city + "." + space + "I am" + 
+ space + age + space + "years old." + space + "I'm feeling" + space + mood + space + "and my favorite food is" + space + food + ".";
 
 });
